32a2911cc2781d145f7f2d33e06792d347ab1c4f
Shaun Barnes <shaun.w.barnes@gmail.com>
2025-12-01 15:32:42 -0500

    Day 1 Part 2 Complete

--- day1.cs
+++ day1.cs
@@ -16,6 +16,7 @@ L82
 """;
 
 int password = 0;
+int password2 = 0;
 
 LinkedList<int> dial = new();
 
@@ -37,41 +38,66 @@ var lines = Helpers.Input.ReadInputFromFile(data, puzzle);
 
 foreach (var line in lines)
 {
+	bool passedZero = false;
+
 	if (line[0] == 'L')
-		current = TurnLeft(current, int.Parse(line[1..]));
+		(current, passedZero) = TurnLeft(current, int.Parse(line[1..]));
 	else
-		current = TurnRight(current, int.Parse(line[1..]));
+		(current, passedZero) = TurnRight(current, int.Parse(line[1..]));
 	
-	System.Console.WriteLine($"The dial is rotated {line} to point at {current.Value}.");
+	System.Console.Write($"The dial is rotated {line} to point at {current.Value}");
+
+	if (passedZero)
+	{
+		System.Console.Write("; during this rotation, it points at 0 once");
+	}
+
+	System.Console.WriteLine(".");
 
 	if (current.Value == 0)
+	{
 		password++;
+	}
+		
 }
 
 System.Console.WriteLine($"\nThe password is {password}");
+System.Console.WriteLine($"\nThe password using method 0x434C49434B is {password2}");
 
-LinkedListNode<int> TurnLeft(LinkedListNode<int> c, int steps)
+(LinkedListNode<int>, bool) TurnLeft(LinkedListNode<int> c, int steps)
 {
+	bool passed = false;
+	bool first = true;
+
 	for (int i = steps; i > 0; i--)
 	{
 		if (c.Value == 0)
 		{
+			password2++;
+
 			if (dial.Last == null)
 				throw new InvalidOperationException("dial.Last is null");
 			c = dial.Last;
+			if (first == false)
+				passed = true;
+
 			continue;
 		}
 
 		if (c.Previous == null)
 				throw new InvalidOperationException("c.Previous is null");
 		c = c.Previous;
+		first = false;
 	}
 
-	return c;
+	return (c, passed);
 }
 
-LinkedListNode<int> TurnRight(LinkedListNode<int> c, int steps)
+(LinkedListNode<int>, bool) TurnRight(LinkedListNode<int> c, int steps)
 {
+	bool passed = false;
+	bool first = true;
+
 	for (int i = steps; i > 0; i--)
 	{
 		if (c.Value == 99)
@@ -82,10 +108,18 @@ LinkedListNode<int> TurnRight(LinkedListNode<int> c, int steps)
 			continue;
 		}
 
+		if (c.Value == 0)
+		{
+			password2++;
+			if (first == false)
+				passed = true;
+		}
+
 		if (c.Next == null)
 				throw new InvalidOperationException("c.Next is null");
 		c = c.Next;
+		first = false;
 	}
 
-	return c;
+	return (c, passed);
 }
